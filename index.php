7567816937f317f21fd0624d3fdf991af73d2849
renautj <jade.renaut@etu.umontpellier.fr>
2023-11-13 12:02:58 +0100

    ex7

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
 $date = new \DateTime();
+
 echo "<p>Hello world!</p>";
+
+echo "<p>Nous sommes le <strong>{$date->format('j F Y')}</strong> et il est <strong>{$date->format('H:i')}</strong></p>";
 
 ?>
